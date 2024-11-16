8b894622aed3cc125dc8a69e5fed3bfa1ff7d20c
PeterYaoNYU <yy4108@nyu.edu>
2024-11-16 16:32:35 +0800

    extend the period longer

--- scripts/net_delay/normal_dist.sh
+++ scripts/net_delay/normal_dist.sh
@@ -14,11 +14,11 @@ sudo tc qdisc add dev vlan1139 root netem delay 10ms 5ms distribution normal
 sudo tc qdisc add dev vlan1120 root netem delay 10ms 5ms distribution normal
 
 
-sudo tc qdisc add dev vlan102 root netem delay 1ms 2ms distribution normal
-sudo tc qdisc add dev vlan105 root netem delay 2ms 5ms distribution normal
-sudo tc qdisc add dev vlan107 root netem delay 2ms 5ms distribution normal
-sudo tc qdisc add dev vlan109 root netem delay 2ms 5ms distribution normal
-sudo tc qdisc add dev vlan110 root netem delay 2ms 5ms distribution normal
+sudo tc qdisc add dev vlan102 root netem delay 1ms 1ms distribution normal
+sudo tc qdisc add dev vlan105 root netem delay 2ms 1ms distribution normal
+sudo tc qdisc add dev vlan107 root netem delay 2ms 1ms distribution normal
+sudo tc qdisc add dev vlan109 root netem delay 2ms 1ms distribution normal
+sudo tc qdisc add dev vlan110 root netem delay 2ms 1ms distribution normal
 
 sudo tc qdisc del dev vlan102 root
 sudo tc qdisc del dev vlan105 root
