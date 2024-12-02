fa3abf20a38505458d31c6d6f850030b48a907a7
PeterYaoNYU <yy4108@nyu.edu>
2024-12-02 19:51:02 +0800

    change IP bind address

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -46,12 +46,21 @@ processes = {}
 #     {"host": "pc603.emulab.net", "port": 29442},
 # ]
 
+# nodes = [
+#     {"host": "c220g1-031113.wisc.cloudlab.us", "port": 22},
+#     {"host": "c220g1-031130.wisc.cloudlab.us", "port": 22},
+#     {"host": "c220g1-031108.wisc.cloudlab.us", "port": 22},
+#     {"host": "c220g1-031125.wisc.cloudlab.us", "port": 22},
+#     {"host": "c220g1-031129.wisc.cloudlab.us", "port": 22},
+# ]
+
 nodes = [
-    {"host": "c220g1-031113.wisc.cloudlab.us", "port": 22},
-    {"host": "c220g1-031130.wisc.cloudlab.us", "port": 22},
-    {"host": "c220g1-031108.wisc.cloudlab.us", "port": 22},
-    {"host": "c220g1-031125.wisc.cloudlab.us", "port": 22},
-    {"host": "c220g1-031129.wisc.cloudlab.us", "port": 22},
+    {"host": "c220g2-010828.wisc.cloudlab.us", "port": 26610},
+    {"host": "c220g2-010823.wisc.cloudlab.us", "port": 26610},
+    {"host": "c220g2-010823.wisc.cloudlab.us", "port": 26611},
+    {"host": "c220g2-010828.wisc.cloudlab.us", "port": 26611},
+    {"host": "c220g2-010823.wisc.cloudlab.us", "port": 26612},
+    # {"host": "c240g5-110103.wisc.cloudlab.us", "port": 26612},
 ]
 
 
