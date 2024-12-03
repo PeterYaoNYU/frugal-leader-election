c59258104b7ebe9f131a5c2bd5bd3f871bc71621
PeterYaoNYU <yy4108@nyu.edu>
2024-12-03 14:13:51 +0800

    change safety margin

--- scripts/net_delay/normal_dist.sh
+++ scripts/net_delay/normal_dist.sh
@@ -1,8 +1,8 @@
-sudo tc qdisc add dev vlan1140 root netem delay 10ms 5ms distribution normal
-sudo tc qdisc add dev vlan1159 root netem delay 10ms 5ms distribution normal
-sudo tc qdisc add dev vlan1191 root netem delay 10ms 5ms distribution normal
-sudo tc qdisc add dev vlan1213 root netem delay 10ms 5ms distribution normal
-sudo tc qdisc add dev vlan1221 root netem delay 10ms 5ms distribution normal
+sudo tc qdisc add dev vlan1140 root netem delay 2ms ms distribution normal
+sudo tc qdisc add dev vlan1159 root netem delay 2ms 1ms distribution normal
+sudo tc qdisc add dev vlan1191 root netem delay 2ms 1ms distribution normal
+sudo tc qdisc add dev vlan1213 root netem delay 2ms 1ms distribution normal
+sudo tc qdisc add dev vlan1221 root netem delay 2ms 1ms distribution normal
 
 
 
